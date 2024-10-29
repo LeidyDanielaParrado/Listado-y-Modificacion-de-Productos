9ae0476d98a416c9ef116d9ef66b716871844f72
Leidy Parrado <danielaparrado14@gmail.com>
2024-10-29 14:34:01 -0500

    cambio de imagen vanesa

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
         <p><a href="https://www.linkedin.com/in/daniela-parrado-451a6a1a3?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app">Linkedin</a></p>
       </div>
       <div className="member">
-        <img src="src/images/vanessa.jfif" alt="LauraB" />
+        <img src="src/images/vanessa.png" alt="LauraB" />
         <h3>Laura Bornacelli Parra</h3>
         <p><a href="https://github.com/Bornacelli">Github</a></p>
         <p><a href="https://linkedin.com/in/miembro2">linkedin</a></p>
